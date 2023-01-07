d4f176e22ba52718da3c042a74df749b8949ded3
shimizu-issei <isseichuo@gmail.com>
2023-01-07 18:42:17 +0900

    fix

---
+++ AtCoder Beginner Contest-A/ABC0232A - QQ solver.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Scanf("%dx%d", &a, &b)
+	fmt.Println(a * b)
+}
+
+/*問題文のURL
+https://atcoder.jp/contests/abc232/tasks/abc232_a
+*/
